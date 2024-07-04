b27f718f901002b625aa21ee33c53df7c8f8c037
ProgasiFox <96259001+ProgasiFox@users.noreply.github.com>
2024-07-04 18:42:08 +0300

    Очистка мусора.

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -2,33 +2,9 @@ import React from 'react';
 
 // MaterialUI
 import { styled, useTheme } from '@mui/material/styles';
-import { Drawer } from "@mui/material";
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-
-// Other vital visual components.
-import Header from "./general_components/Header"
-import Menu from "./general_components/Menu";
 import Layout from "./general_components/Layout"
-
-// MaterialUI
 import { Container, Grid, Card, CardContent, CardMedia, Typography as MuiTypography } from '@mui/material';
-import KeyIcon from '@mui/icons-material/Key';
 
 const drawerWidth = 240;
 
