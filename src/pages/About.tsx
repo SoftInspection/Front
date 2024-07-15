06839b2bf9c9a71048050c14fea00e94dfc766b4
ProgasiFox <96259001+ProgasiFox@users.noreply.github.com>
2024-07-15 12:32:27 +0300

    /about обновлён, добавлена информация о лицензировании.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import { Container, Typography, Box, Card, CardContent, CardMedia, Grid, List, ListItem, ListItemText } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 import { Link } from "react-router-dom";
 import Layout from './general_components/Layout';
 import Developers from './Developers';
+import IMG from "./general_components/creativecommonsimg.png";
 
 const About: React.FC = () => {
     return (
@@ -17,8 +20,10 @@ const About: React.FC = () => {
                         О нас
                     </Typography>
                     <Typography variant="body1" paragraph>
-                        Добро пожаловать в <Typography color="secondary" component={Link} to="/">Sub2Soft!</Typography> Мы предлагаем уникальный маркетплейс, посвящённый исключительно программному обеспечению. Наш магазин отличается высоким качеством и безопасностью: каждая программа проверяется на наличие вирусов, что гарантирует вам безопасность и надёжность.
+                        Добро пожаловать в <Typography color="secondary" component={Link} to="/">Sub2Soft</Typography>, выполненный командой разработчиков SoftInspection! Мы предлагаем уникальный маркетплейс, посвящённый исключительно программному обеспечению. Наш магазин отличается высоким качеством и безопасностью: каждая программа проверяется на наличие вирусов, что гарантирует вам безопасность и надёжность.
                     </Typography>
+
+                    <Alert severity="success">Sub2Soft позволяет использование в экспериментальных целях, включая ЛОКАЛЬНЫЕ внутренние изменения и работа с исходными материалами (после письменного согласия авторов сайта).</Alert>
                 </Box>
                 <Box sx={{ my: 4 }}>
                     <Typography variant="h4" component="h2" gutterBottom>
@@ -56,6 +61,15 @@ const About: React.FC = () => {
                 </Typography>
                 <hr />
                 <Box sx={{ my: 4 }}>
+                    <Typography variant="h4" component="h2" gutterBottom>
+                        Наши продукты
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Мы предлагаем широкий ассортимент программного обеспечения, которое удовлетворяет разнообразные потребности наших клиентов. Наши продукты включают инструменты для управления проектами, разработки программного обеспечения и многое другое... <Typography color="secondary" component={Link} to="/">Взгляните сами!</Typography>
+                    </Typography>
+                </Box>
+                <hr />
+                <Box sx={{ my: 4 }}>
                     <Box sx={{ my: 4 }}>
                         <Typography variant="h4" component="h2" gutterBottom>
                             Наша команда
@@ -66,58 +80,76 @@ const About: React.FC = () => {
                         <Developers />
                     </Box>
                 </Box>
+                <hr />
+                <Alert severity="warning">Защита Sub2Soft является действительной.</Alert>
                 <Box sx={{ my: 4 }}>
                     <Typography variant="h4" component="h2" gutterBottom>
-                        Наши продукты
+                        Ознакомительный Характер Информации
                     </Typography>
                     <Typography variant="body1" paragraph>
-                        Мы предлагаем широкий ассортимент программного обеспечения, которое удовлетворяет разнообразные потребности наших клиентов. Наши продукты включают инструменты для управления проектами, разработки программного обеспечения и многое другое... <Typography color="secondary" component={Link} to="/">Взгляните сами!</Typography>
+                        Вся информация, представленная на данном веб-сайте, носит исключительно ознакомительный характер. Наша цель — предоставить пользователям полезные сведения о программном обеспечении и его возможностях. Мы стремимся к тому, чтобы информация была точной и актуальной, однако не гарантируем абсолютную точность и полноту предоставленных данных.
                     </Typography>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12} sm={6}>
-                            <Card>
-                                <CardMedia
-                                    component="img"
-                                    height="150"
-                                    image="https://via.placeholder.com/300x150"
-                                    alt="Product 1"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Продукт 1
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Описание продукта 1. Этот продукт помогает вам в управлении проектами и повышении эффективности работы.
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <Card>
-                                <CardMedia
-                                    component="img"
-                                    height="150"
-                                    image="https://via.placeholder.com/300x150"
-                                    alt="Product 2"
-                                />
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        Продукт 2
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary">
-                                        Описание продукта 2. Этот продукт предоставляет мощные инструменты для разработки программного обеспечения.
-                                    </Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Grid>
                 </Box>
+                <Box sx={{ my: 4 }}>
+                    <Typography variant="h4" component="h2" gutterBottom>
+                        Использование Импортных Модулей, Элементов, API etc.
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Импортные модули и элементы, использованные на этом сайте, были взяты на открытой основе. Мы используем компоненты и библиотеки с открытым исходным кодом, что позволяет нам разрабатывать функциональные и современные решения, следуя лучшим практикам индустрии. Мы благодарны сообществу разработчиков за их вклад и поддержку.
+                    </Typography>
+                </Box>
+                <Box sx={{ my: 4 }}>
+                    <Typography variant="h4" component="h2" gutterBottom>
+                        Политика Конфиденциальности
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Сайт не обрабатывает и не хранит персональные данные пользователей. Мы уважаем вашу конфиденциальность и не собираем информацию, которая могла бы идентифицировать вас лично. Все взаимодействия с сайтом происходят в анонимном режиме, что обеспечивает безопасность и приватность наших пользователей.
+                    </Typography>
+                </Box>
+                <Box sx={{ my: 4 }}>
+                    <Typography variant="h4" component="h2" gutterBottom>
+                        Авторские Права и Лицензия
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Все материалы и компоненты, использованные на сайте, защищены авторским правом и распространяются в соответствии с условиями лицензии <Typography color="secondary" component="span">Creative Commons CC BY-NC-ND</Typography>. Это означает, что вы можете свободно скачивать и делиться нашим контентом при соблюдении следующих условий:
+                    </Typography>
+
+
+                    <Stack sx={{ width: '100%' }} spacing={2}>
+                        <Alert severity="warning">Атрибуция: Вы должны указать авторство нашего контента.</Alert>
+                        <Alert severity="error">Некомерческое использование: Вы не можете использовать наш контент в коммерческих целях.</Alert>
+                        <Alert severity="error">Без производных произведений: Вы не можете изменять, преобразовывать или создавать новые произведения на основе нашего контента.</Alert>
+                        <Alert severity="success">Локальное использование полностью рарешено.</Alert>
+                        <Alert severity="info">Отказ от Гарантий: Программное обеспечение предоставляется "как есть", без каких-либо гарантий. Авторы проекта (SoftInspection) не несут ответственности за любые возможные проблемы или убытки, возникающие в результате использования программного обеспечения.</Alert>
+                        <hr />
+                        <img src={IMG} alt="CREATIVE COMMONS CC BY-NC-ND" />
+                        <hr />
+                    </Stack>
+
+                </Box>
+                <Box sx={{ my: 4 }}>
+                    <Typography variant="h4" component="h2" gutterBottom>
+                        Ознакомительный Характер Информации
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Вся информация, представленная на данном веб-сайте, носит исключительно ознакомительный характер. Наша цель — предоставить пользователям полезные сведения о программном обеспечении и его возможностях. Мы стремимся к тому, чтобы информация была точной и актуальной, однако не гарантируем абсолютную точность и полноту предоставленных данных.
+                    </Typography>
+                </Box>
+                <Box sx={{ my: 4 }}>
+                    <Typography variant="h4" component="h2" gutterBottom>
+                        Ознакомительный Характер Информации
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        Вся информация, представленная на данном веб-сайте, носит исключительно ознакомительный характер. Наша цель — предоставить пользователям полезные сведения о программном обеспечении и его возможностях. Мы стремимся к тому, чтобы информация была точной и актуальной, однако не гарантируем абсолютную точность и полноту предоставленных данных.
+                    </Typography>
+                </Box>
+                <hr />
                 <Box sx={{ my: 4 }}>
                     <Typography variant="h4" component="h2" gutterBottom>
                         Контакты
                     </Typography>
                     <Typography variant="body1" paragraph>
-                        Если у вас есть вопросы или предложения, пожалуйста, свяжитесь с нами. Мы всегда рады помочь!
+                        Если у вас есть вопросы или предложения, пожалуйста, <Typography color="secondary" component={Link} to="/feedback">свяжитесь с нами</Typography>! Мы всегда рады помочь!
                     </Typography>
                 </Box>
             </Container>
